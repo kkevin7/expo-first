06995e65a6070d26c9521f8eb9ec2bddf62e6e8e
Kevin Martinez <kkevinmartinez7@gmail.com>
2023-04-18 11:29:15 -0600

    FlatList component

--- App.js
+++ App.js
@@ -2,113 +2,43 @@ import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
   Dimensions,
-  Button,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
 } from "react-native";
 
-const width = Dimensions.get("window").width;
+const data = [
+  { key: "1", name: "Kevin" },
+  { key: "2", name: "Jose" },
+  { key: "3", name: "Dayana" },
+  { key: "4", name: "Django" },
+  { key: "5", name: "Benji" },
+  { key: "6", name: "Kevin" },
+  { key: "7", name: "Jose" },
+  { key: "8", name: "Dayana" },
+  { key: "9", name: "Django" },
+  { key: "10", name: "Benji" },
+  { key: "11", name: "Kevin" },
+  { key: "12", name: "Jose" },
+  { key: "13", name: "Dayana" },
+  { key: "14", name: "Django" },
+  { key: "15", name: "Benji" },
+  { key: "16", name: "Kevin" },
+  { key: "17", name: "Jose" },
+  { key: "18", name: "Dayana" },
+  { key: "19", name: "Django" },
+  { key: "20", name: "Benji" },
+];
 
 export default function App() {
-  const [text, setText] = useState("Nice Day :)");
-  const [submit, setSubmit] = useState("");
-
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <Text>Texto: {submit}</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your text"
-          onChangeText={(t) => setText(t)}
-          defaultValue={text}
-        />
-        <TouchableHighlight
-          underlayColor={"#999"}
-          activeOpacity={0.2}
-          onPress={() => {
-            setSubmit(text);
-            alert("Text Succesfully Sent");
-          }}
-        >
-          <Text>Aceptar</Text>
-        </TouchableHighlight>
-        <TouchableNativeFeedback
-          background={TouchableNativeFeedback.Ripple("#00f", true)}
-          onPress={() => {
-            setSubmit(text);
-            alert("Text Succesfully Sent");
-          }}
-        >
-          <View style={styles.view}>
-            <Text>Aceptar</Text>
-          </View>
-        </TouchableNativeFeedback>
-        <TouchableOpacity
-          style={styles.touchableOpacity}
-          onPress={() => {
-            setSubmit(text);
-            alert("Text Succesfully Sent");
-          }}
-        >
-          <View style={styles.view}>
-            <Text>Aceptar</Text>
-          </View>
-        </TouchableOpacity>
-      </ScrollView>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
     </View>
   );
 }
@@ -117,25 +47,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    alignItems: "stretch",
     justifyContent: "center",
-  },
-  view: {
-    height: 40,
-    width: 300,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  touchableOpacity: {
-    backgroundColor: "#EEE",
-  },
-  input: {
-    height: 40,
-    backgroundColor: "#ccc",
-    borderBottomWidth: 1,
-    width: width,
+    paddingTop: 22,
   },
   scrollView: {
     width: Dimensions.get("window").width,
+  },
+  item: {
+    padding: 10,
+    fontSize: 22,
+    height: 50,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
   },
 });
