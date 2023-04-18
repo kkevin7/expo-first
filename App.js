2fcb204c674afdd576f709281c117671bc4adcad
Kevin Martinez <kkevinmartinez7@gmail.com>
2023-04-18 10:37:05 -0600

    Button component

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+  Button,
+} from "react-native";
 
 const width = Dimensions.get("window").width;
 
 export default function App() {
   const [text, setText] = useState("Nice Day :)");
+  const [submit, setSubmit] = useState("");
 
   return (
     <View style={styles.container}>
-      <Text>Texto: {text}</Text>
+      <Text>Texto: {submit}</Text>
       <TextInput
         style={styles.input}
         placeholder="Enter your text"
         onChangeText={(t) => setText(t)}
         defaultValue={text}
+      />
+      <Button
+        onPress={() => {
+          setSubmit(text);
+          alert("Text Succesfully Sent");
+        }}
+        title="Aceptar"
       />
     </View>
   );
