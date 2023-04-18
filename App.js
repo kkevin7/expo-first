901e6fa9e0387e059e24a02808a91370e0b47c03
Kevin Martinez <kkevinmartinez7@gmail.com>
2023-04-18 12:33:24 -0600

    using image component

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import {
-  ScrollView,
-  Dimensions,
-  StyleSheet,
-  View,
-  ActivityIndicator,
-} from "react-native";
+import { ScrollView, Dimensions, StyleSheet, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ActivityIndicator size={"large"} color={"#0000ff"} />
+      <Image style={styles.photo} source={require("./assets/icon.png")} />
+      <Image
+        style={styles.photo2}
+        source={{ uri: "http://placekitten.com/200/200" }}
+      />
     </View>
   );
 }
@@ -25,7 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "stretch",
+    alignItems: "center",
     justifyContent: "center",
     paddingTop: 22,
   },
@@ -38,5 +36,13 @@ const styles = StyleSheet.create({
     height: 50,
     borderBottomColor: "#ccc",
     borderBottomWidth: 1,
+  },
+  photo: {
+    height: 60,
+    width: 60,
+  },
+  photo2: {
+    height: 200,
+    width: 200,
   },
 });
